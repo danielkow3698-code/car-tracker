fc3bc8a02f9229b8a418b5760049859bd6197b5e
Daniel <danielkow3698@gmail.com>
2026-05-28 21:04:12 +0800

    PWA: iOS meta/apple-touch-icon, start_url /

--- pages/_app.js
+++ pages/_app.js
@@ -76,7 +76,10 @@ export default function App({ Component, pageProps }) {
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
         <meta name="apple-mobile-web-app-title" content="Car Tracker" />
         <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
         <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/icon-192.png" />
+        <link rel="apple-touch-startup-image" href="/icon-512.png" />
       </Head>
       <Component
         {...pageProps}
